114dad691e773c8db8b34e60764d49ebdcd9322c
lukejones <47660116+BetaDraconis@users.noreply.github.com>
2019-11-25 11:58:04 +0000

    MTDSA-4354: Added connector for TriggerBsas endpoint.

---
+++ test/v1/connectors/TriggerBsasConnectorSpec.scala
@@ -0,0 +1,56 @@
+/*
+ * Copyright 2019 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package v1.connectors
+
+import mocks.MockAppConfig
+import uk.gov.hmrc.domain.Nino
+import v1.mocks.MockHttpClient
+import v1.models.outcomes.ResponseWrapper
+import v1.models.request.TriggerBsasRequestData
+import v1.fixtures.TriggerBsasRequestBodyFixtures._
+import v1.models.response.triggerBsas.TriggerBsasResponse
+
+import scala.concurrent.Future
+
+class TriggerBsasConnectorSpec extends ConnectorSpec {
+
+  val nino = Nino("AA123456A")
+  val id = "f2fb30e5-4ab6-4a29-b3c1-c7264259ff1c"
+
+  class Test extends MockHttpClient with MockAppConfig {
+    val connector: TriggerBsasConnector = new TriggerBsasConnector(http = mockHttpClient, appConfig = mockAppConfig)
+
+    val desRequestHeaders: Seq[(String, String)] = Seq("Environment" -> "des-environment", "Authorization" -> s"Bearer des-token")
+    MockedAppConfig.desBaseUrl returns baseUrl
+    MockedAppConfig.desToken returns "des-token"
+    MockedAppConfig.desEnvironment returns "des-environment"
+  }
+
+  "triggerBsas" must {
+    val request = TriggerBsasRequestData(nino,seBody)
+
+    "post an empty body and return the result" in new Test {
+      val outcome = Right(ResponseWrapper(correlationId, TriggerBsasResponse(id)))
+
+      MockedHttpClient
+        .post(s"$baseUrl/income-tax/adjustable-summary-calculation/${nino.nino}", seBody, "Environment" -> "des-environment", "Authorization" -> s"Bearer des-token")
+        .returns(Future.successful(outcome))
+
+      await(connector.triggerBsas(request)) shouldBe outcome
+    }
+  }
+}
