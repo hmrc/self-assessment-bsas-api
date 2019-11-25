d5153fdff1cc99e099f62bc41c30f66993b63595
Narendra Vijayarao <26090262+narendravijayarao@users.noreply.github.com>
2019-11-25 17:03:49 +0000

    Added service layer (#14)

---
+++ test/v1/services/TriggerBsasServiceSpec.scala
@@ -0,0 +1,87 @@
+/*
+ * Copyright 2019 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package v1.services
+
+import support.UnitSpec
+import uk.gov.hmrc.domain.Nino
+import uk.gov.hmrc.http.HeaderCarrier
+import v1.controllers.EndpointLogContext
+import v1.models.errors._
+import v1.models.outcomes.ResponseWrapper
+import v1.fixtures.TriggerBsasRequestBodyFixtures._
+import v1.mocks.connectors.MockTriggerBsasConnector
+import v1.models.request.TriggerBsasRequestData
+import v1.models.response.triggerBsas.TriggerBsasResponse
+
+import scala.concurrent.Future
+
+import scala.concurrent.ExecutionContext.Implicits.global
+
+class TriggerBsasServiceSpec extends UnitSpec {
+
+  private val nino = Nino("AA123456A")
+  val id = "f2fb30e5-4ab6-4a29-b3c1-c7264259ff1c"
+  private val correlationId = "X-123"
+
+  val request = TriggerBsasRequestData(nino, seBody)
+
+  val response = TriggerBsasResponse(id)
+
+  trait Test extends MockTriggerBsasConnector {
+    implicit val hc: HeaderCarrier = HeaderCarrier()
+    implicit val logContext: EndpointLogContext = EndpointLogContext("controller", "triggerBsas")
+
+    val service = new TriggerBsasService(mockConnector)
+  }
+
+  "triggerBsas" should {
+    "return a valid response" when {
+      "a valid request is supplied" in new Test {
+        MockTriggerBsasConnector.triggerBsas(request)
+          .returns(Future.successful(Right(ResponseWrapper(correlationId, response))))
+
+        await(service.triggerBsas(request)) shouldBe Right(ResponseWrapper(correlationId, response))
+      }
+    }
+
+    "return error response" when {
+
+      def serviceError(desErrorCode: String, error: MtdError): Unit =
+        s"a $desErrorCode error is returned from the service" in new Test {
+
+          MockTriggerBsasConnector.triggerBsas(request)
+            .returns(Future.successful(Left(ResponseWrapper(correlationId, DesErrors.single(DesErrorCode(desErrorCode))))))
+
+          await(service.triggerBsas(request)) shouldBe Left(ErrorWrapper(Some(correlationId), error))
+        }
+
+      val input = Seq(
+
+        ("INVALID_TAXABLE_ENTITY_ID", NinoFormatError),
+        ("ACCOUNTING_PERIOD_NOT_ENDED", RuleAccountingPeriodNotEndedError),
+        ("OBLIGATIONS_NOT_MET", RulePeriodicDataIncompleteError),
+        ("NO_ACCOUNTING_PERIOD", RuleNoAccountingPeriodError),
+        ("NOT_FOUND", NotFoundError),
+        ("INVALID_PAYLOAD", DownstreamError),
+        ("SERVER_ERROR", DownstreamError),
+        ("SERVICE_UNAVAILABLE", DownstreamError)
+      )
+
+      input.foreach(args => (serviceError _).tupled(args))
+    }
+  }
+}
