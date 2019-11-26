b8d8775bc6a6fe8a1c4de15dddbdd7f0ffed45dc
Adrian Jones <nboaram@users.noreply.github.com>
2019-11-26 10:40:56 +0000

    MTDSA-4368 Request Parser and Validation

---
+++ test/v1/controllers/requestParsers/validators/validations/ListBSASTypeOfBusinessValidationSpec.scala
@@ -0,0 +1,40 @@
+/*
+ * Copyright 2019 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package v1.controllers.requestParsers.validators.validations
+
+import support.UnitSpec
+
+class ListBSASTypeOfBusinessValidationSpec extends UnitSpec {
+
+  private val typeOfBusiness = "uk-property-fhl"
+  private val badTypeOfBusiness = "humidity farmer"
+
+  "validate" should {
+    "return no errors" when {
+      "a valid type of business is provided" in {
+        ListBSASTypeOfBusinessValidation.validate(typeOfBusiness) shouldBe Nil
+      }
+    }
+
+    "return an error" when {
+      "an invalid type of business is provided" in {
+        ListBSASTypeOfBusinessValidation.validate(badTypeOfBusiness) shouldBe List(TypeOfBusinessFormatError)
+      }
+    }
+  }
+
+}
