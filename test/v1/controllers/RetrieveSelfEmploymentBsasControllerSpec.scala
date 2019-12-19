12cebe47154a5a9cb0f648bb9f2d57536978ba1b
David Walters <32767569+david-walters-HMRC@users.noreply.github.com>
2019-12-19 14:15:07 +0000

    MTDSA-4444 added controller for retrieve self assessment BSAS endpoint (#65)

---
+++ test/v1/controllers/RetrieveSelfEmploymentBsasControllerSpec.scala
@@ -0,0 +1,159 @@
+/*
+ * Copyright 2019 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package v1.controllers
+
+import play.api.libs.json.Json
+import play.api.mvc.Result
+import uk.gov.hmrc.domain.Nino
+import uk.gov.hmrc.http.HeaderCarrier
+import v1.fixtures.RetrieveSelfEmploymentBsasFixtures._
+import v1.mocks.hateoas.MockHateoasFactory
+import v1.mocks.requestParsers.MockRetrieveSelfEmploymentRequestParser
+import v1.mocks.services.{MockEnrolmentsAuthService, MockMtdIdLookupService, MockRetrieveSelfEmploymentBsasService}
+import v1.models.errors._
+import v1.models.hateoas.{HateoasWrapper, Link}
+import v1.models.hateoas.Method.{GET, POST}
+import v1.models.outcomes.ResponseWrapper
+import v1.models.request.{RetrieveSelfEmploymentBsasRawData, RetrieveSelfEmploymentBsasRequestData}
+import v1.models.response.retrieveBsas.selfEmployment.RetrieveSelfAssessmentsAdjustmentHateoasData
+
+import scala.concurrent.ExecutionContext.Implicits.global
+import scala.concurrent.Future
+
+class RetrieveSelfEmploymentBsasControllerSpec extends ControllerBaseSpec
+  with MockEnrolmentsAuthService
+  with MockMtdIdLookupService
+  with MockRetrieveSelfEmploymentRequestParser
+  with MockRetrieveSelfEmploymentBsasService
+  with MockHateoasFactory {
+
+  trait Test {
+    val hc = HeaderCarrier()
+
+    val controller = new RetrieveSelfEmploymentBsasController(
+      authService = mockEnrolmentsAuthService,
+      lookupService = mockMtdIdLookupService,
+      requestParser = mockRequestParser,
+      service = mockService,
+      hateoasFactory = mockHateoasFactory,
+      cc = cc
+    )
+
+    MockedMtdIdLookupService.lookup(nino).returns(Future.successful(Right("test-mtd-id")))
+    MockedEnrolmentsAuthService.authoriseUser()
+  }
+
+  private val nino = "AA123456A"
+  private val correlationId = "X-123"
+  private val bsasId = "f2fb30e5-4ab6-4a29-b3c1-c7264259ff1c"
+  private val adjustedMtdStatus = Some("true")
+  private val adjustedDesStatus = Some("03")
+
+  private val request = RetrieveSelfEmploymentBsasRequestData(Nino(nino), bsasId, adjustedDesStatus)
+  private val requestRawData = RetrieveSelfEmploymentBsasRawData(nino, bsasId, adjustedMtdStatus)
+
+  val testHateoasLinkSelf = Link(href = s"/individuals/self-assessment/adjustable-summary/$nino/self-employment/$bsasId",
+    method = GET, rel = "self")
+
+  val testHateoasLinkAdjustSubmit = Link(href = s"/individuals/self-assessment/adjustable-summary/$nino/self-employment/$bsasId/adjust",
+    method = POST, rel = "submit-summary-adjustments")
+
+  "retrieve" should {
+    "return successful hateoas response for self-assessment with status OK" when {
+      "a valid request supplied" in new Test {
+
+        MockRetrieveSelfEmploymentRequestParser
+          .parse(requestRawData)
+          .returns(Right(request))
+
+        MockRetrieveSelfEmploymentBsasService
+          .retrieveBsas(request)
+          .returns(Future.successful(Right(ResponseWrapper(correlationId, retrieveBsasResponseModelAdjusted))))
+
+        MockHateoasFactory
+          .wrap(retrieveBsasResponseModelAdjusted, RetrieveSelfAssessmentsAdjustmentHateoasData(nino, bsasId))
+          .returns(HateoasWrapper(retrieveBsasResponseModelAdjusted , Seq(testHateoasLinkSelf, testHateoasLinkAdjustSubmit))
+        )
+
+        val result: Future[Result] = controller.retrieve(nino, bsasId, adjustedMtdStatus)(fakeGetRequest)
+
+        status(result) shouldBe OK
+        contentAsJson(result) shouldBe Json.parse(hateoasResponseForAdjustedSelfAssessment(nino, bsasId))
+        header("X-CorrelationId", result) shouldBe Some(correlationId)
+      }
+    }
+
+    "return the error as per spec" when {
+      "parser errors occur" must {
+        def errorsFromParserTester(error: MtdError, expectedStatus: Int): Unit = {
+          s"a ${error.code} error is returned from the parser" in new Test {
+
+            MockRetrieveSelfEmploymentRequestParser
+              .parse(requestRawData)
+              .returns(Left(ErrorWrapper(Some(correlationId), error, None)))
+
+            val result: Future[Result] = controller.retrieve(nino, bsasId, adjustedMtdStatus)(fakeGetRequest)
+
+            status(result) shouldBe expectedStatus
+            contentAsJson(result) shouldBe Json.toJson(error)
+            header("X-CorrelationId", result) shouldBe Some(correlationId)
+          }
+        }
+
+        val input = Seq(
+          (AdjustedStatusFormatError, BAD_REQUEST),
+          (NinoFormatError, BAD_REQUEST),
+          (BsasIdFormatError, BAD_REQUEST)
+        )
+
+        input.foreach(args => (errorsFromParserTester _).tupled(args))
+      }
+
+      "service errors occur" must {
+        def serviceErrors(mtdError: MtdError, expectedStatus: Int): Unit = {
+          s"a $mtdError error is returned from the service" in new Test {
+
+            MockRetrieveSelfEmploymentRequestParser
+              .parse(requestRawData)
+              .returns(Right(request))
+
+            MockRetrieveSelfEmploymentBsasService
+              .retrieveBsas(request)
+              .returns(Future.successful(Left(ErrorWrapper(Some(correlationId), mtdError))))
+
+            val result: Future[Result] = controller.retrieve(nino, bsasId, adjustedMtdStatus)(fakeGetRequest)
+
+            status(result) shouldBe expectedStatus
+            contentAsJson(result) shouldBe Json.toJson(mtdError)
+            header("X-CorrelationId", result) shouldBe Some(correlationId)
+          }
+        }
+
+        val input = Seq(
+          (NinoFormatError, BAD_REQUEST),
+          (BsasIdFormatError, BAD_REQUEST),
+          (DownstreamError, INTERNAL_SERVER_ERROR),
+          (RuleNoAdjustmentsMade, FORBIDDEN),
+          (NotFoundError, NOT_FOUND),
+          (RuleNotSelfEmployment, FORBIDDEN)
+        )
+
+        input.foreach(args => (serviceErrors _).tupled(args))
+      }
+    }
+  }
+}
