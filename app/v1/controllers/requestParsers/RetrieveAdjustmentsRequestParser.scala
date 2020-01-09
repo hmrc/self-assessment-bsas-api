3ce791e11eb99ae95a8939b7084aea566e07d336
lukejones <47660116+BetaDraconis@users.noreply.github.com>
2020-01-09 16:04:55 +0000

    MTDSA 4542: Fixing merge issues.

--- app/v1/controllers/requestParsers/RetrieveAdjustmentsRequestParser.scala
+++ app/v1/controllers/requestParsers/RetrieveAdjustmentsRequestParser.scala
@@ -18,10 +18,10 @@ package v1.controllers.requestParsers
 
 import javax.inject.Inject
 import uk.gov.hmrc.domain.Nino
-import v1.controllers.requestParsers.validators.RetrieveSelfEmploymentAdjustmentsValidator
+import v1.controllers.requestParsers.validators.RetrieveAdjustmentsValidator
 import v1.models.request.{RetrieveAdjustmentsRawData, RetrieveAdjustmentsRequestData}
 
-class RetrieveSelfEmploymentAdjustmentsRequestParser @Inject()(val validator: RetrieveSelfEmploymentAdjustmentsValidator)
+class RetrieveAdjustmentsRequestParser @Inject()(val validator: RetrieveAdjustmentsValidator)
   extends RequestParser[RetrieveAdjustmentsRawData, RetrieveAdjustmentsRequestData] {
 
   override protected def requestFor(data: RetrieveAdjustmentsRawData): RetrieveAdjustmentsRequestData =
