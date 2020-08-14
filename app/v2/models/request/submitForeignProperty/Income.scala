14c5e027f9755f50c760676e77f735d7f0f7473f
DreamcastKnight <56401347+DreamcastKnight@users.noreply.github.com>
2020-08-14 15:25:11 +0100

    models started

---
+++ app/v2/models/request/submitForeignProperty/Income.scala
@@ -0,0 +1,28 @@
+/*
+ * Copyright 2020 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package v2.models.request.submitForeignProperty
+
+import play.api.libs.json.{Json, OFormat}
+
+case class Income(rentIncome: Option[BigDecimal],
+                  premiumsOfLeaseGrant: Option[BigDecimal],
+                  foreignTaxTakenOff: Option[BigDecimal],
+                  otherPropertyIncome: Option[BigDecimal])
+
+object Income {
+  implicit val format: OFormat[Income] = Json.format[Income]
+}
