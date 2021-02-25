c684110fd520ae090068c703e7f69dea9092a6c9
DreamcastKnight <56401347+DreamcastKnight@users.noreply.github.com>
2021-02-25 10:58:50 +0000

    mapping alignments

--- app/v2/services/SubmitUkPropertyBsasService.scala
+++ app/v2/services/SubmitUkPropertyBsasService.scala
@@ -50,24 +50,22 @@ class SubmitUkPropertyBsasService @Inject()(connector: SubmitUkPropertyBsasConne
   }
 
   private def mappingDesToMtdError: Map[String, MtdError] = Map(
-    "INVALID_TAXABLE_ENTITY_ID"   -> NinoFormatError,
-    "INVALID_CALCULATION_ID"      -> BsasIdFormatError,
-    "INVALID_PAYLOAD"             -> DownstreamError,
-    "INVALID_PAYLOAD_REMOTE"      -> DownstreamError,
-    "INVALID_FIELD"               -> RuleTypeOfBusinessError,
-    "INVALID_MONETARY_FORMAT"     -> DownstreamError,
-    "ASC_ID_INVALID"              -> RuleSummaryStatusInvalid,
-    "ASC_ALREADY_SUPERSEDED"      -> RuleSummaryStatusSuperseded,
-    "ASC_ALREADY_ADJUSTED"        -> RuleBsasAlreadyAdjusted,
-    "UNALLOWABLE_VALUE"           -> RuleResultingValueNotPermitted,
-    "BVR_FAILURE_C55316"          -> RuleTypeOfBusinessError,
-    "BVR_FAILURE_C15320"          -> RuleTypeOfBusinessError,
-    "BVR_FAILURE_C55503"          -> RuleOverConsolidatedExpensesThreshold,
-    "BVR_FAILURE_C55508"          -> RulePropertyIncomeAllowanceClaimed,
-    "BVR_FAILURE_C55509"          -> RulePropertyIncomeAllowanceClaimed,
-    "NO_DATA_FOUND"               -> NotFoundError,
-    "SERVER_ERROR"                -> DownstreamError,
-    "SERVICE_UNAVAILABLE"         -> DownstreamError
+    "INVALID_TAXABLE_ENTITY_ID"     -> NinoFormatError,
+    "INVALID_CALCULATION_ID"        -> BsasIdFormatError,
+    "INVALID_PAYLOAD"               -> DownstreamError,
+    "ASC_ID_INVALID"                -> RuleSummaryStatusInvalid,
+    "ASC_ALREADY_SUPERSEDED"        -> RuleSummaryStatusSuperseded,
+    "ASC_ALREADY_ADJUSTED"          -> RuleBsasAlreadyAdjusted,
+    "UNALLOWABLE_VALUE"             -> RuleResultingValueNotPermitted,
+    "INCOMESOURCE_TYPE_NOT_MATCHED" -> RuleTypeOfBusinessError,
+    "BVR_FAILURE_C55316"            -> DownstreamError,
+    "BVR_FAILURE_C15320"            -> DownstreamError,
+    "BVR_FAILURE_C55503"            -> RuleOverConsolidatedExpensesThreshold,
+    "BVR_FAILURE_C55508"            -> RulePropertyIncomeAllowanceClaimed,
+    "BVR_FAILURE_C55509"            -> RulePropertyIncomeAllowanceClaimed,
+    "NO_DATA_FOUND"                 -> NotFoundError,
+    "SERVER_ERROR"                  -> DownstreamError,
+    "SERVICE_UNAVAILABLE"           -> DownstreamError
   )
 
   private def retrieveTypeOfBusiness(body: SubmitUKPropertyBsasRequestBody): TypeOfBusiness = {
