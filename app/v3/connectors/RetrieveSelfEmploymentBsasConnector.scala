3d36d07bce7db4f634ece6a307f78ea2c7908619
Harry Ritchie <111759395+harry-ritchie@users.noreply.github.com>
2023-01-30 12:13:45 +0000

    Header date update

--- app/v3/connectors/RetrieveSelfEmploymentBsasConnector.scala
+++ app/v3/connectors/RetrieveSelfEmploymentBsasConnector.scala
@@ -1,5 +1,5 @@
 /*
- * Copyright 2022 HM Revenue & Customs
+ * Copyright 2023 HM Revenue & Customs
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -17,26 +17,25 @@
 package v3.connectors
 
 import config.AppConfig
-import uk.gov.hmrc.http.{HeaderCarrier, HttpClient}
-import v3.connectors.DownstreamUri.{IfsUri, TaxYearSpecificIfsUri}
+import uk.gov.hmrc.http.{ HeaderCarrier, HttpClient }
+import v3.connectors.DownstreamUri.{ IfsUri, TaxYearSpecificIfsUri }
 import v3.models.request.retrieveBsas.selfEmployment.RetrieveSelfEmploymentBsasRequestData
 import v3.models.response.retrieveBsas.selfEmployment.RetrieveSelfEmploymentBsasResponse
 
-import javax.inject.{Inject, Singleton}
-import scala.concurrent.{ExecutionContext, Future}
+import javax.inject.{ Inject, Singleton }
+import scala.concurrent.{ ExecutionContext, Future }
 
 @Singleton
-class RetrieveSelfEmploymentBsasConnector @Inject()
-      (val http: HttpClient,
-       val appConfig: AppConfig) extends BaseDownstreamConnector {
+class RetrieveSelfEmploymentBsasConnector @Inject()(val http: HttpClient, val appConfig: AppConfig) extends BaseDownstreamConnector {
 
-  def retrieveSelfEmploymentBsas(request: RetrieveSelfEmploymentBsasRequestData)
-                                (implicit hc: HeaderCarrier, ec: ExecutionContext,
-                                 correlationId: String): Future[DownstreamOutcome[RetrieveSelfEmploymentBsasResponse]] = {
+  def retrieveSelfEmploymentBsas(request: RetrieveSelfEmploymentBsasRequestData)(
+      implicit hc: HeaderCarrier,
+      ec: ExecutionContext,
+      correlationId: String): Future[DownstreamOutcome[RetrieveSelfEmploymentBsasResponse]] = {
 
     import v3.connectors.httpparsers.StandardDownstreamHttpParser._
 
-    val nino = request.nino.nino
+    val nino          = request.nino.nino
     val calculationId = request.calculationId
 
     val uri = request.taxYear match {
