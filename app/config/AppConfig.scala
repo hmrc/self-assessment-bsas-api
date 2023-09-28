0c541398f2861be51aa5a93e0cf891b96f21ea4b
Samuel Jacobs <108878134+SamuelJacobsHMRC@users.noreply.github.com>
2023-09-28 11:42:48 +0100

    MTDSA-19432: OasFeatureRewriter

--- app/config/AppConfig.scala
+++ app/config/AppConfig.scala
@@ -25,10 +25,13 @@ import uk.gov.hmrc.play.bootstrap.config.ServicesConfig
 import javax.inject.{Inject, Singleton}
 
 trait AppConfig {
+
   lazy val desDownstreamConfig: DownstreamConfig =
     DownstreamConfig(baseUrl = desBaseUrl, env = desEnv, token = desToken, environmentHeaders = desEnvironmentHeaders)
+
   lazy val ifsDownstreamConfig: DownstreamConfig =
     DownstreamConfig(baseUrl = ifsBaseUrl, env = ifsEnv, token = ifsToken, environmentHeaders = ifsEnvironmentHeaders)
+
   lazy val taxYearSpecificIfsDownstreamConfig: DownstreamConfig =
     DownstreamConfig(baseUrl = tysIfsBaseUrl, env = tysIfsEnv, token = tysIfsToken, environmentHeaders = tysIfsEnvironmentHeaders)
 
@@ -37,104 +40,106 @@ trait AppConfig {
 
   // DES Config
   def desBaseUrl: String
-
   def desEnv: String
-
   def desToken: String
-
   def desEnvironmentHeaders: Option[Seq[String]]
 
   // IFS Config
   def ifsBaseUrl: String
-
   def ifsEnv: String
-
   def ifsToken: String
-
   def ifsEnvironmentHeaders: Option[Seq[String]]
-
   def ifsEnabled: Boolean
 
   // Tax Year Specific (TYS) IFS Config
   def tysIfsBaseUrl: String
-
   def tysIfsEnv: String
-
   def tysIfsToken: String
-
   def tysIfsEnvironmentHeaders: Option[Seq[String]]
 
   // API Config
   def apiGatewayContext: String
-
   def apiStatus(version: Version): String
-
   def isApiDeprecated(version: Version): Boolean = apiStatus(version) == "DEPRECATED"
-
   def featureSwitches: Configuration
-
+  def endpointsEnabled(version: String): Boolean
   def endpointsEnabled(version: Version): Boolean
-
   def confidenceLevelConfig: ConfidenceLevelConfig
-
   def mtdNrsProxyBaseUrl: String
+
+  /** Currently only for OAS documentation.
+    */
+  def apiVersionReleasedInProduction(version: String): Boolean
+
+  /** Currently only for OAS documentation.
+    */
+  def endpointReleasedInProduction(version: String, name: String): Boolean
 
   // V3 Trigger BSAS minimum dates
   def v3TriggerForeignBsasMinimumTaxYear: String
-
   def v3TriggerNonForeignBsasMinimumTaxYear: String
 }
 
 @Singleton
-class AppConfigImpl @Inject()(config: ServicesConfig, configuration: Configuration) extends AppConfig {
+class AppConfigImpl @Inject() (config: ServicesConfig, configuration: Configuration) extends AppConfig {
 
   // MTD ID Lookup Config
   val mtdIdBaseUrl: String = config.baseUrl("mtd-id-lookup")
 
   // Des Config
-  val desBaseUrl: String = config.baseUrl("des")
-  val desEnv: String = config.getString("microservice.services.des.env")
-  val desToken: String = config.getString("microservice.services.des.token")
+  val desBaseUrl: String                         = config.baseUrl("des")
+  val desEnv: String                             = config.getString("microservice.services.des.env")
+  val desToken: String                           = config.getString("microservice.services.des.token")
   val desEnvironmentHeaders: Option[Seq[String]] = configuration.getOptional[Seq[String]]("microservice.services.des.environmentHeaders")
 
   // IFS Config
-  val ifsBaseUrl: String = config.baseUrl("ifs")
-  val ifsEnv: String = config.getString("microservice.services.ifs.env")
-  val ifsToken: String = config.getString("microservice.services.ifs.token")
-  val ifsEnabled: Boolean = config.getBoolean("microservice.services.ifs.enabled")
+  val ifsBaseUrl: String                         = config.baseUrl("ifs")
+  val ifsEnv: String                             = config.getString("microservice.services.ifs.env")
+  val ifsToken: String                           = config.getString("microservice.services.ifs.token")
+  val ifsEnabled: Boolean                        = config.getBoolean("microservice.services.ifs.enabled")
   val ifsEnvironmentHeaders: Option[Seq[String]] = configuration.getOptional[Seq[String]]("microservice.services.ifs.environmentHeaders")
 
   // Tax Year Specific (TYS) IFS Config
-  val tysIfsBaseUrl: String = config.baseUrl("tys-ifs")
-  val tysIfsEnv: String = config.getString("microservice.services.tys-ifs.env")
-  val tysIfsToken: String = config.getString("microservice.services.tys-ifs.token")
+  val tysIfsBaseUrl: String                         = config.baseUrl("tys-ifs")
+  val tysIfsEnv: String                             = config.getString("microservice.services.tys-ifs.env")
+  val tysIfsToken: String                           = config.getString("microservice.services.tys-ifs.token")
   val tysIfsEnvironmentHeaders: Option[Seq[String]] = configuration.getOptional[Seq[String]]("microservice.services.tys-ifs.environmentHeaders")
 
   // Api Config
-  val apiGatewayContext: String = config.getString("api.gateway.context")
-  val mtdNrsProxyBaseUrl: String = config.baseUrl("mtd-api-nrs-proxy")
+  val apiGatewayContext: String                    = config.getString("api.gateway.context")
+  val mtdNrsProxyBaseUrl: String                   = config.baseUrl("mtd-api-nrs-proxy")
   val confidenceLevelConfig: ConfidenceLevelConfig = configuration.get[ConfidenceLevelConfig](s"api.confidence-level-check")
 
   // V3 Trigger BSAS minimum dates
-  val v3TriggerForeignBsasMinimumTaxYear: String = config.getString("v3TriggerForeignBsasMinimumTaxYear")
+  val v3TriggerForeignBsasMinimumTaxYear: String    = config.getString("v3TriggerForeignBsasMinimumTaxYear")
   val v3TriggerNonForeignBsasMinimumTaxYear: String = config.getString("v3TriggerNonForeignBsasMinimumTaxYear")
 
-  def apiStatus(version: Version): String = config.getString(s"api.${version.name}.status")
-
-  def featureSwitches: Configuration = configuration.getOptional[Configuration](s"feature-switch").getOrElse(Configuration.empty)
-
+  def apiStatus(version: Version): String         = config.getString(s"api.${version.name}.status")
+  def featureSwitches: Configuration              = configuration.getOptional[Configuration](s"feature-switch").getOrElse(Configuration.empty)
+  def endpointsEnabled(version: String): Boolean  = config.getBoolean(s"api.$version.endpoints.enabled")
   def endpointsEnabled(version: Version): Boolean = config.getBoolean(s"api.${version.name}.endpoints.enabled")
+  def apiVersionReleasedInProduction(version: String): Boolean = config.getBoolean(s"api.$version.endpoints.api-released-in-production")
+
+  def endpointReleasedInProduction(version: String, name: String): Boolean = {
+    val versionReleasedInProd = apiVersionReleasedInProduction(version)
+    val path                  = s"api.$version.endpoints.released-in-production.$name"
+
+    val conf = configuration.underlying
+    if (versionReleasedInProd && conf.hasPath(path)) config.getBoolean(path) else versionReleasedInProd
+  }
+
 }
 
 trait FixedConfig {
   // Minimum tax year for MTD
-  val minimumTaxYear = 2018
+  val minimumTaxYear     = 2018
   val listMinimumTaxYear = 2020
 }
 
 case class ConfidenceLevelConfig(confidenceLevel: ConfidenceLevel, definitionEnabled: Boolean, authValidationEnabled: Boolean)
 
 object ConfidenceLevelConfig {
+
   implicit val configLoader: ConfigLoader[ConfidenceLevelConfig] = (rootConfig: Config, path: String) => {
     val config = rootConfig.getConfig(path)
     ConfidenceLevelConfig(
@@ -143,4 +148,5 @@ object ConfidenceLevelConfig {
       authValidationEnabled = config.getBoolean("auth-validation.enabled")
     )
   }
+
 }
--- app/config/DocumentationController.scala
+++ app/config/DocumentationController.scala
@@ -16,9 +16,9 @@
 
 package config
 
-import controllers.Assets
+import config.rewriters.DocumentationRewriters
+import controllers.RewriteableAssets
 import definition.ApiDefinitionFactory
-import play.api.http.HttpErrorHandler
 import play.api.libs.json.Json
 import play.api.mvc.{Action, AnyContent, ControllerComponents}
 import uk.gov.hmrc.play.bootstrap.backend.controller.BackendController
@@ -26,14 +26,21 @@ import uk.gov.hmrc.play.bootstrap.backend.controller.BackendController
 import javax.inject.{Inject, Singleton}
 
 @Singleton
-class DocumentationController @Inject()(apiDefinition: ApiDefinitionFactory, cc: ControllerComponents, assets: Assets, errorHandler: HttpErrorHandler)
-  extends BackendController(cc) {
+class DocumentationController @Inject() (
+    selfAssessmentApiDefinition: ApiDefinitionFactory,
+    docRewriters: DocumentationRewriters,
+    assets: RewriteableAssets,
+    cc: ControllerComponents
+) extends BackendController(cc) {
 
   def definition(): Action[AnyContent] = Action {
-    Ok(Json.toJson(apiDefinition.definition))
+    Ok(Json.toJson(selfAssessmentApiDefinition.definition))
   }
 
-  def asset(version: String, file: String): Action[AnyContent] = {
-    assets.at(s"/public/api/conf/$version", file)
+  def asset(version: String, filename: String): Action[AnyContent] = {
+    val path      = s"/public/api/conf/$version"
+    val rewriters = docRewriters.rewriteables.flatMap { _.maybeRewriter(version, filename) }
+    assets.rewriteableAt(path, filename, rewriters)
   }
+
 }
