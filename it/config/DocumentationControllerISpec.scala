3d983f43d6ad5ff8d91e8d25370dbb1c51f42e0b
SamuelJacobsHMRC <108878134+SamuelJacobsHMRC@users.noreply.github.com>
2023-09-29 14:12:37 +0100

    MTDSA-19513: Fix Wrong Accept Header value - update OAS and integration test (#425)
    * MTDSA-19513: Fix Wrong Accept Header value - update OAS and integration test
    
    * MTDSA-19513: Updated DocumentationControllerISpec

--- it/config/DocumentationControllerISpec.scala
+++ it/config/DocumentationControllerISpec.scala
@@ -18,20 +18,19 @@ package config
 import io.swagger.v3.parser.OpenAPIV3Parser
 import play.api.http.Status
 import play.api.http.Status.OK
-import play.api.libs.json.{JsValue, Json}
+import play.api.libs.json.Json
 import play.api.libs.ws.WSResponse
 import routing.{Version3, Version4}
 import support.IntegrationBaseSpec
-import uk.gov.hmrc.auth.core.ConfidenceLevel
 
 import scala.util.Try
 
 class DocumentationControllerISpec extends IntegrationBaseSpec {
 
-  val config: AppConfig                = app.injector.instanceOf[AppConfig]
-  val confidenceLevel: ConfidenceLevel = config.confidenceLevelConfig.confidenceLevel
+  private val config          = app.injector.instanceOf[AppConfig]
+  private val confidenceLevel = config.confidenceLevelConfig.confidenceLevel
 
-  val apiDefinitionJson: JsValue = Json.parse(s"""
+  private val apiDefinitionJson = Json.parse(s"""
        |{
        |  "scopes": [
        |    {
@@ -90,6 +89,19 @@ class DocumentationControllerISpec extends IntegrationBaseSpec {
           openAPI.getInfo.getTitle shouldBe "Business Source Adjustable Summary (MTD)"
         }
         openAPI.getInfo.getVersion shouldBe version.toString
+      }
+
+      s"return the documentation with the correct accept header for version $version" in {
+        val response = get(s"/api/conf/${version.name}/common/headers.yaml")
+        val body     = response.body[String]
+
+        val headerRegex = """(?s).*?application/vnd\.hmrc\.(\d+\.\d+)\+json.*?""".r
+        val header      = headerRegex.findFirstMatchIn(body)
+        header.isDefined shouldBe true
+
+        val versionFromHeader = header.get.group(1)
+        versionFromHeader shouldBe version.name
+
       }
     }
   }
