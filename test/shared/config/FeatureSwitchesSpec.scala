3215a7f54c9261bab788152643fb3ae2d37afd12
jeremystone <6453521+jeremystone@users.noreply.github.com>
2024-06-07 09:55:24 +0100

    NOJIRA - allow easier testing of feature switches
    All feature switches should behave the same. In order to be able to test the correct behaviour in APIs where there are multiple feature switches and reduce unnecessary boilerplate, this PR proposes a  `FeatureSwitchesBehaviour` trait that has test cases to check this in a general case (for 'enabled', 'released-in-production' and other flavours).
    
    The FeatureSwitchesSpec uses this (to test the general purpose `isEnabled`/`isEnabledInProduction`) methods; and the API-specific BsasFeatureSwitchesSpec
    does the same for those methods particular to BSAS.

--- test/shared/config/FeatureSwitchesSpec.scala
+++ test/shared/config/FeatureSwitchesSpec.scala
@@ -19,35 +19,18 @@ package shared.config
 import play.api.Configuration
 import shared.UnitSpec
 
-class FeatureSwitchesSpec extends UnitSpec {
+class FeatureSwitchesSpec extends UnitSpec with FeatureSwitchesBehaviour[FeatureSwitches] {
 
-  private case class TestFeatureSwitches(protected val featureSwitchConfig: Configuration) extends FeatureSwitches
+  override def featureSwitches(configuration: Configuration): FeatureSwitches = new FeatureSwitches {
+    override protected val featureSwitchConfig: Configuration = configuration
+  }
 
-  "FeatureSwitches" should {
-    "be true" when {
-      "absent from the config" in {
-        val configuration   = Configuration.empty
-        val featureSwitches = TestFeatureSwitches(configuration)
+  "isEnabled" should {
+    behave like aFeatureSwitchWithKey("some-feature.enabled", _.isEnabled("some-feature"))
+  }
 
-        featureSwitches.isEnabled("some-feature") shouldBe true
-      }
-
-      "enabled" in {
-        val configuration   = Configuration("some-feature.enabled" -> true)
-        val featureSwitches = TestFeatureSwitches(configuration)
-
-        featureSwitches.isEnabled("some-feature") shouldBe true
-      }
-    }
-
-    "be false" when {
-      "disabled" in {
-        val configuration   = Configuration("some-feature.enabled" -> false)
-        val featureSwitches = TestFeatureSwitches(configuration)
-
-        featureSwitches.isEnabled("some-feature") shouldBe false
-      }
-    }
+  "isReleasedInProduction" should {
+    behave like aFeatureSwitchWithKey("some-feature.released-in-production", _.isReleasedInProduction("some-feature"))
   }
 
 }
