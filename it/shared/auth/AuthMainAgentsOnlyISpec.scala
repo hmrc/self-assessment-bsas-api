84163c6971a01e8f64c2214f86c395ba3f25270c
Matt Stephens <97457093+mattstephens-hmrc@users.noreply.github.com>
2024-08-08 16:19:28 +0100

    MTDSA-25635 Make Auth ITs easier to roll out (#512)
    * MTDSA-24955 EMA secondary agents support
    
    * MTDSA-25635 Auth ITs refactor
    
    * MTDSA-25635 Auth ITs refactor

--- it/shared/auth/AuthMainAgentsOnlyISpec.scala
+++ it/shared/auth/AuthMainAgentsOnlyISpec.scala
@@ -1,38 +1,38 @@
-/*
- * Copyright 2022 HM Revenue & Customs
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIED OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-
-package auth
+package shared.auth
 
 import com.github.tomakehurst.wiremock.stubbing.StubMapping
 import play.api.http.HeaderNames.ACCEPT
 import play.api.http.Status.{FORBIDDEN, INTERNAL_SERVER_ERROR, OK}
-import play.api.libs.json.{JsValue, Json}
+import play.api.libs.json.JsValue
 import play.api.libs.ws.{WSRequest, WSResponse}
 import play.api.test.Helpers.AUTHORIZATION
 import shared.models.errors.{ClientOrAgentNotAuthorisedError, InternalError}
 import shared.services.{AuditStub, AuthStub, DownstreamStub, MtdIdLookupStub}
 import support.IntegrationBaseSpec
-import v6.bsas.trigger.def1.model.Def1_TriggerBsasFixtures
-import v6.common.model.TypeOfBusiness
 
-class AuthPrimaryAgentsOnlyISpec extends IntegrationBaseSpec {
+abstract class AuthMainAgentsOnlyISpec extends IntegrationBaseSpec {
 
-  private val callingApiVersion = "6.0"
+  /** The API's latest version, e.g. "1.0".
+    */
+  protected val callingApiVersion: String
 
-  private val supportingAgentsNotAllowedEndpoint = "trigger-bsas"
+  /** As the IT supplies the "supported" config below, this can be any endpoint IF there's no actual "main agents only" endpoint in the API.
+    */
+  protected val supportingAgentsNotAllowedEndpoint: String
+
+  protected def sendMtdRequest(request: WSRequest): WSResponse
+
+  protected val mtdUrl: String
+
+  protected val downstreamUri: String
+
+  protected val maybeDownstreamResponseJson: Option[JsValue]
+
+  protected val downstreamHttpMethod: DownstreamStub.HTTPMethod = DownstreamStub.POST
+
+  protected val downstreamSuccessStatus: Int = OK
+
+  protected val expectedMtdSuccessStatus: Int = OK
 
   /** One endpoint where supporting agents are allowed.
     */
@@ -41,17 +41,7 @@ class AuthPrimaryAgentsOnlyISpec extends IntegrationBaseSpec {
       s"api.supporting-agent-endpoints.$supportingAgentsNotAllowedEndpoint" -> "false"
     ) ++ super.servicesConfig
 
-  private val nino = "AA123456A"
-
-  private val mtdUrl = s"/$nino/trigger"
-
-  private val requestJson = Json.obj(
-    "accountingPeriod" -> Json.obj("startDate" -> "2019-01-01", "endDate" -> "2019-10-31"),
-    "typeOfBusiness"   -> TypeOfBusiness.`self-employment`.toString,
-    "businessId"       -> "XAIS12345678901"
-  )
-
-  private val downstreamResponse: JsValue = Json.parse(Def1_TriggerBsasFixtures.downstreamResponse)
+  protected val nino = "AA123456A"
 
   "Calling an endpoint that only allows primary agents" when {
     "the client is the primary agent" should {
@@ -64,12 +54,12 @@ class AuthPrimaryAgentsOnlyISpec extends IntegrationBaseSpec {
           AuthStub.authorisedWithPrimaryAgentEnrolment()
 
           DownstreamStub
-            .when(DownstreamStub.POST, downstreamUri)
-            .thenReturn(OK, downstreamResponse)
+            .when(downstreamHttpMethod, downstreamUri)
+            .thenReturn(downstreamSuccessStatus, maybeDownstreamResponseJson)
         }
 
-        val response: WSResponse = sendMtdRequest()
-        response.status shouldBe OK
+        val response: WSResponse = sendMtdRequest(request())
+        response.status shouldBe expectedMtdSuccessStatus
       }
     }
 
@@ -84,7 +74,7 @@ class AuthPrimaryAgentsOnlyISpec extends IntegrationBaseSpec {
           AuthStub.unauthorisedForPrimaryAgentEnrolment()
         }
 
-        val response: WSResponse = sendMtdRequest()
+        val response: WSResponse = sendMtdRequest(request())
 
         response.status shouldBe FORBIDDEN
         response.body should include(ClientOrAgentNotAuthorisedError.message)
@@ -103,7 +93,7 @@ class AuthPrimaryAgentsOnlyISpec extends IntegrationBaseSpec {
           AuthStub.unauthorisedNotLoggedIn()
         }
 
-        val response: WSResponse = sendMtdRequest()
+        val response: WSResponse = sendMtdRequest(request())
         response.status shouldBe FORBIDDEN
       }
     }
@@ -117,7 +107,7 @@ class AuthPrimaryAgentsOnlyISpec extends IntegrationBaseSpec {
           AuthStub.unauthorisedOther()
         }
 
-        val response: WSResponse = sendMtdRequest()
+        val response: WSResponse = sendMtdRequest(request())
         response.status shouldBe FORBIDDEN
         response.body should include(ClientOrAgentNotAuthorisedError.message)
       }
@@ -131,7 +121,7 @@ class AuthPrimaryAgentsOnlyISpec extends IntegrationBaseSpec {
           MtdIdLookupStub.error(nino, INTERNAL_SERVER_ERROR)
         }
 
-        val response: WSResponse = sendMtdRequest()
+        val response: WSResponse = sendMtdRequest(request())
         response.status shouldBe INTERNAL_SERVER_ERROR
         response.body should include(InternalError.message)
       }
@@ -145,20 +135,19 @@ class AuthPrimaryAgentsOnlyISpec extends IntegrationBaseSpec {
           MtdIdLookupStub.error(nino, FORBIDDEN)
         }
 
-        val response: WSResponse = sendMtdRequest()
+        val response: WSResponse = sendMtdRequest(request())
         response.status shouldBe FORBIDDEN
         response.body should include(ClientOrAgentNotAuthorisedError.message)
       }
     }
   }
 
-  private trait Test {
+  protected trait Test {
 
     def setupStubs(): StubMapping
 
-    def sendMtdRequest(): WSResponse = await(request.post(requestJson))
-
-    private def request: WSRequest = {
+    protected def request(): WSRequest = {
+      AuthStub.resetAll()
       setupStubs()
       buildRequest(mtdUrl)
         .withHttpHeaders(
@@ -167,7 +156,6 @@ class AuthPrimaryAgentsOnlyISpec extends IntegrationBaseSpec {
         )
     }
 
-    def downstreamUri: String = s"/income-tax/adjustable-summary-calculation/$nino"
   }
 
 }
