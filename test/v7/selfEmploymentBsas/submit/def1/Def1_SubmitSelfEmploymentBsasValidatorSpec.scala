e7d479cb0e9d223819136c74e315fe86e2749ab9
Ed Cope <87376121+EdCope@users.noreply.github.com>
2025-03-28 13:51:30 +0000

    MTDSA-29533: API#1874 & #1518 | Removal of "Allowable" from field names (#569)

--- test/v7/selfEmploymentBsas/submit/def1/Def1_SubmitSelfEmploymentBsasValidatorSpec.scala
+++ test/v7/selfEmploymentBsas/submit/def1/Def1_SubmitSelfEmploymentBsasValidatorSpec.scala
@@ -119,9 +119,9 @@ class Def1_SubmitSelfEmploymentBsasValidatorSpec extends UnitSpec with JsonError
 
         List(
           "/expenses",
-          "/expenses/costOfGoodsAllowable",
-          "/expenses/paymentsToSubcontractorsAllowable",
-          "/expenses/wagesAndStaffCostsAllowable"
+          "/expenses/costOfGoods",
+          "/expenses/paymentsToSubcontractors",
+          "/expenses/wagesAndStaffCosts"
         ).foreach(path => testWith(mtdRequestJson.replaceWithEmptyObject(path), path))
 
         List(
@@ -190,21 +190,21 @@ class Def1_SubmitSelfEmploymentBsasValidatorSpec extends UnitSpec with JsonError
         List(
           "/income/turnover",
           "/income/other",
-          "/expenses/costOfGoodsAllowable",
-          "/expenses/paymentsToSubcontractorsAllowable",
-          "/expenses/wagesAndStaffCostsAllowable",
-          "/expenses/carVanTravelExpensesAllowable",
-          "/expenses/premisesRunningCostsAllowable",
-          "/expenses/maintenanceCostsAllowable",
-          "/expenses/adminCostsAllowable",
-          "/expenses/interestOnBankOtherLoansAllowable",
-          "/expenses/financeChargesAllowable",
-          "/expenses/irrecoverableDebtsAllowable",
-          "/expenses/professionalFeesAllowable",
-          "/expenses/depreciationAllowable",
-          "/expenses/otherExpensesAllowable",
-          "/expenses/advertisingCostsAllowable",
-          "/expenses/businessEntertainmentCostsAllowable",
+          "/expenses/costOfGoods",
+          "/expenses/paymentsToSubcontractors",
+          "/expenses/wagesAndStaffCosts",
+          "/expenses/carVanTravelExpenses",
+          "/expenses/premisesRunningCosts",
+          "/expenses/maintenanceCosts",
+          "/expenses/adminCosts",
+          "/expenses/interestOnBankOtherLoans",
+          "/expenses/financeCharges",
+          "/expenses/irrecoverableDebts",
+          "/expenses/professionalFees",
+          "/expenses/depreciation",
+          "/expenses/otherExpenses",
+          "/expenses/advertisingCosts",
+          "/expenses/businessEntertainmentCosts",
           "/additions/costOfGoodsDisallowable",
           "/additions/paymentsToSubcontractorsDisallowable",
           "/additions/wagesAndStaffCostsDisallowable",
